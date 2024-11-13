0a34f2f72af3fd2e6f63273b5d9473d4b7004525
Joseph Karasek <joseph.karasek@gmail.com>
2024-11-12 20:59:23 -0800

    update favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@
 // @todo use the Docusaurus link throughout custom code
 // import Link from "@docusaurus/Link";
 // import Heading from "@theme/Heading";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+// import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import { HomepageBannerSection } from "@site/src/sections/HomepageBannerSection/HomepageBannerSection";
 import { HomepageIntroSection } from "@site/src/sections/HomepageIntroSection/HomepageIntroSection";
@@ -10,10 +10,9 @@ import { HomepageSkillsSection } from "@site/src/sections/HomepageSkillsSection/
 import { HomepageHighlightsSection } from "@site/src/sections/HomepageHighlightsSection/HomepageHighlightsSection";
 
 const Home = () => {
-  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`Home`}
       description="Description will go into a meta tag in <head />"
     >
       <main className="home">
